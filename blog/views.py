259b12d515ed63f5d5c56bbe7100f115c1156050
Maciej Tuszynski <xtzl9j78@students.codeinstitute.net>
2024-12-08 23:49:49 +0000

    deployment settings

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def my_blog(request):
-return HttpResponse("Hello, Blog!")
+        return HttpResponse("Hello, Blog!")
