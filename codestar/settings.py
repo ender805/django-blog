0321eaec0558e876a9ef7d7241045666420d719f
Maciej Tuszynski <xtzl9j78@students.codeinstitute.net>
2024-12-13 23:16:46 +0000

    update

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-ender805-djangoblog-m5fdpk79hl7.ws.codeinstitute-ide.net",'.herokuapp.com']
 
